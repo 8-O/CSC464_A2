e5db63f4a4a011109c4686372cf5759d97b310e1
Thor <thorr@uvic.ca>
2018-11-23 22:17:15 -0800

    updated byz.py to work properly

--- byz.py
+++ byz.py
@@ -7,22 +7,26 @@
 
   resource: https://marknelson.us/posts/2007/07/23/byzantine.html
 """
+import copy
 from random import randint
 from collections import defaultdict
 
 leader_id = 0
 processes = dict()
 traitor_ids = dict()
-children = defaultdict(lambda : defaultdict())
-ranked_paths = defaultdict(lambda : defaultdict(lambda : defaultdict()))
+children = defaultdict(lambda: dict())
+ranked_paths = defaultdict(lambda: defaultdict(list))
 init_ids = dict()
 n = 7 #num processes
-m = 4 #num messages
+m = 10 #num messages
 t = 2 #num traitors
 
 def main():
     for i in range(n):
         traitor_ids[i] = False
+        init_ids[i] = True
+    create_children(m, n, init_ids, leader_id)
+    for i in range(n):
         processes[i] = process(i)
     traitors = 0
     while traitors < t:
@@ -32,20 +36,34 @@ def main():
         else:
             traitor_ids[i] = True
             traitors +=1
-    for i in range(m):
+    for i in range(m+1):
         for j in range(n):
             processes[j].send_messages(i, processes)
-    for i in range(len(processes)):
+    for i in range(n):
         if processes[i].pid is leader_id:
-            print("Leader ", end='')
-        print("Process %s " % (i), end='')
-        if traitor_ids[i]:
-            print("(traitor) ", end='')
-        print("has decided on value %s" % (processes[i].decide()))
+            print("Leader Process %s " % (i), end='')
+            if traitor_ids[i]:
+                print("is a traitor.")
+            else:
+                print("commanded value %s." % (processes[i].nodes[''].input))
+        else:
+            print("Process %s has decided on value %s" % (i, processes[i].decide()))
     return
 
+def create_children(m, n, pids, source, current_path ='', recursion =0):
+    pids[source] = False
+    current_path += str(source)
+    count = 0
+    ranked_paths[recursion][source].append(current_path)
+    if recursion < m:
+        for i in range(n):
+            if pids[i]:
+                create_children(m, n, copy.copy(pids), i, current_path, recursion+1)
+                children[current_path][count] = current_path + str(i)
+                count += 1
+
 class node():
-    def __init__(self, input, output):
+    def __init__(self, input =0, output =0):
         self.input = input
         self.output = output
 
@@ -53,42 +71,30 @@ class process():
     def __init__(self, id):
         self.pid = id
         self.nodes = dict()
-        if len(children) is 0:
-            self.create_children(m, n, init_ids, leader_id)
         if self.pid is leader_id:
-            self.value = randint(0,1)
-            self.nodes[''] = node(self.value, '?')
+            value = randint(0,1)
+            self.nodes[''] = node(value, '?')
 
-    def confirm_value(self, value, destination):
+    def confirm_value(self, value):
         if traitor_ids[self.pid]:
             return randint(0,1)
-        if self.pid is leader_id:
-            return self.value
-        return value
+        else:
+            return value
 
-    def create_children(self, m, n, pids, source, current_path ='', recursion =0):
-        pids[source] = False
-        current_path += str(source)
-        count = 0
-        ranked_paths[recursion][source][count] = current_path
-        if recursion < m:
-            for i in range(len(pids)):
-                if pids[i]:
-                    self.create_children(m, n, ids, i, current_path, recursion+1)
-                    children[current_path][count] = current_path + str(i)
-                    count += 1
-
-    def receive_message(self, path, tar_node):
-        self.nodes[str(path)] = tar_node
+    def receive_message(self, path, node):
+        self.nodes[str(path)] = node
 
     def send_messages(self, round, processes):
         for i in range(len(ranked_paths[round][self.pid])):
             source_path = ranked_paths[round][self.pid][i]
-            source_path = source_path[0:-1]
+            source_path = source_path[:-1]
             source_node = self.nodes[str(source_path)]
             for j in range(n):
-                value = self.confirm_value(source_node.input, j)
-                processes[j].receive_message(ranked_paths[round][self.pid][i], 0)
+                value = self.confirm_value(source_node.input)
+                #print('Sending from process ' + str(self.pid) + ' to ' + str(j) + ':', end='')
+                #print('{' + str(value) + ', ' + ranked_paths[round][self.pid][i] + ', ' + str('?') + '},',end='')
+                #print('getting value from source_node ' + source_path + ' value ' + str(value))
+                processes[j].receive_message(ranked_paths[round][self.pid][i], node(value,'?'))
 
     def decide(self):
         if self.pid is leader_id:
@@ -96,15 +102,15 @@ class process():
         for i in range(n):
             for j in range(len(ranked_paths[m][i])):
                 path = ranked_paths[m][i][j]
-                tnode = self.nodes[str(path)]
-                tnode.output = tnode.input
+                node = self.nodes[str(path)]
+                node.output = node.input
         round = m -1
         while round >= 0:
             for i in range(n):
                 for j in range(len(ranked_paths[round][i])):
                     path = ranked_paths[round][i][j]
-                    tnode = self.nodes[str(path)]
-                    tnode.output = self.get_majority(path)
+                    node = self.nodes[str(path)]
+                    node.output = self.get_majority(path)
             round -=1
         top_path = ranked_paths[0][leader_id][0]
         top_node = self.nodes[str(top_path)]
@@ -112,9 +118,12 @@ class process():
 
     def get_majority(self, path):
         tallies = [0,0]
-        for child in children[path]:
-            node_x = self.nodes[str(child)]
-            tallies[node_x.output] +=1
+        for key, child in children[path].items():
+            node = self.nodes[str(child)]
+            if node.output is 1:
+                tallies[1] +=1
+            elif node.output is 0:
+                tallies[0] +=1
         if tallies[0] < tallies[1]:
             return 1
         else:
